fd7ef3c77e22a0cad62a72bb98b7d04888e0cc42
Simon Skinner <codewith@vultuk.me>
2015-01-06 12:18:43 +0000

    Added docbloc to permissions contract

--- src/Clix/Authenticator/Contracts/Permissions.php
+++ src/Clix/Authenticator/Contracts/Permissions.php
@@ -1,8 +1,23 @@
 <?php namespace Clix\Authenticator\Contracts;
 
+/**
+ * Interface Permissions
+ * @package Clix\Authenticator\Contracts
+ * @author Simon Skinner <s.skinner@clix.co.uk>
+ */
 interface Permissions
 {
+
+    /**
+     * @param $permission
+     * @return mixed
+     * @author Simon Skinner <s.skinner@clix.co.uk>
+     */
     public function hasAccess($permission);
 
+    /**
+     * @return mixed
+     * @author Simon Skinner <s.skinner@clix.co.uk>
+     */
     public function listAll();
 } 
