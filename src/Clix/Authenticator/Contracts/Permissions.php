49d69dec7aae7c0c9a378ee1523dc845bb535567
Simon Skinner <codewith@vultuk.me>
2015-01-06 12:25:17 +0000

    Added correct docbloc

--- src/Clix/Authenticator/Contracts/Permissions.php
+++ src/Clix/Authenticator/Contracts/Permissions.php
@@ -1,6 +1,17 @@
 <?php namespace Clix\Authenticator\Contracts;
 
+/**
+ * Interface Permissions
+ * @package Clix\Authenticator\Contracts
+ * @author Simon Skinner <s.skinner@clix.co.uk>
+ */
 interface Permissions
 {
+
+    /**
+     * @param $permission
+     * @return mixed
+     * @author Simon Skinner <s.skinner@clix.co.uk>
+     */
     public function hasAccess($permission);
 } 
